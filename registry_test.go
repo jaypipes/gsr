dc6128ffd1986be45a429ee8e2cc0d75e4b5ef05
Jay Pipes <jaypipes@gmail.com>
2017-04-19 16:41:37 -0400

    Add functional New() success test

--- registry_test.go
+++ registry_test.go
@@ -25,3 +25,27 @@ func TestNewBadAddress(t *testing.T) {
         t.Error("Expected nil, but got %v.", r)
     }
 }
+
+func TestFunctionalNewSuccess(t *testing.T) {
+    testEps, found := os.LookupEnv("GSR_TEST_ETCD_ENDPOINTS")
+    if ! found {
+        t.Skip("GSRT_TEST_ETCD_ENDPOINTS not set. Skipping functional test.")
+    }
+
+    orig, found := os.LookupEnv("GSR_ETCD_ENDPOINTS")
+    if ! found {
+        defer os.Unsetenv("GSR_ETCD_ENDPOINTS")
+    } else {
+        defer os.Setenv("GSR_ETCD_ENDPOINTS", orig)
+    }
+
+    os.Setenv("GSR_ETCD_ENDPOINTS", testEps)
+
+    r, err := New()
+    if err != nil {
+        t.Error("Expected nil, but got %v.", err)
+    }
+    if r == nil {
+        t.Error("Expected *gsr.Registry, but got nil.")
+    }
+}
